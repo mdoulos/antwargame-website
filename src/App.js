dae3894733fad0a144f9f7dc7965f7af9f718b1d
MDoulos <mdcpu@mdoulos.com>
2023-10-05 10:24:02 -0400

    Header and Footer drafts finished.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
-import { Navbar } from './components'
+import { Navbar, Footer } from './components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Footer />
     </div>
   );
 }
